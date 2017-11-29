15da81fa654e1707329c44e9aae4f583e70cc2e1
Brian Lin <9brian9@gmail.com>
2017-11-29 21:21:06 +1300

    stable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-server.get(/.*/, restify.serveStatic({ 'directory': '.', 'default': 'index.html' }));
+//server.get(/.*/, restify.serveStatic({ 'directory': '.', 'default': 'index.html' }));
 
 
 // Create chat connector for communicating with the Bot Framework Service
